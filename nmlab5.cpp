7c1f3d36b2bfcc94aa8c97f2ed6a39c4ab12fbe0
amritkumargalden <amritkumargaldentamang@gmail.com>
2025-02-10 22:15:35 +0545

    Update nmlab5.c

--- nmlab5.cpp
+++ nmlab5.cpp
@@ -1,37 +1,25 @@
-//To find the value of f(x) at the defined value of x of range x0 to xn by using Lagrange interpolation.
-
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	int n,i,j;
-	float x1, x[10], y[10], sum=0, product;
-	
-	cout<<"Enter the value of n:"<<endl;
-	cin>>n;
-	
-	for(i=0; i<n; i++)
-	{
-		cout<<"Enter the value of x["<<i<<"] y["<<i<<"]:";
-		cin>>x[i]>>y[i];
-		cout<<endl;		
-	}
-	
-	cout<<"Enter the value for which y to determine:\n";
-	cin>>x1;
-	
-	for(i=0; i<=n; i++)
-	{
-		product=1;
-		for(j=0; j<=n; j++)
-		{
-			if(i!=j)
-				product*=(x1-x[j])/(x[1]-x[j]);
-		}
-		sum=sum + product*y[i];
-	}
-	cout<<"THE SUM IS "<<sum;
-	
-	return 0;
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int n,i;
+float x[10],y[10],sx=0.0,sy=0.0,sx2=0.0,sxy=0.0,a,b;
+clrscr();
+printf("enter the no terms:");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+printf("a[%d]b[%d]:",i,i);
+scanf("%f%f",&x[i],&y[i]);
+printf("%f %f",x[i],y[i]);
+}
+for(i=1;i<=n;i++){
+ sx=sx+x[i];
+ sy=sy+y[i];
+ sxy=sxy+(x[i]*y[i]);
+ sx2=sx2+(x[i]*x[i]);
+}
+printf("%f %f %f %f",sx,sy,sxy,sx2);
+a=((n*sxy)-(sx*sy))/((n*sx2)-(sx*sx));
+b=((sx2*sy)-(sx*sy))/((n*sx2)-(sx*sx));
+printf("the value of a=%f and b=%f",a,b);
+getch();
 }
